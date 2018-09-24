409c15993b7634b2cf1aa8f3cfe9815fdf278da6
Vulcan942 <ncnishant9868@gmail.com>
2018-09-24 23:07:51 +0530

    project12

---
+++ Project12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Project12
+{
+    class Program
+    {
+        private string name=" ";
+        public string Name{
+            get{
+                return name;
+            }
+            set{
+                name = value;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine();
+            p.Name="Bruce Wayne";
+            Console.WriteLine("Hello, {0}",p.name);
+            Console.WriteLine();
+        }
+    }
+}
