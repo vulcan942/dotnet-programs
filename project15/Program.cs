45fadae9227aba9f157c0f628e77235e06afd47d
Vulcan942 <ncnishant9868@gmail.com>
2018-09-24 23:48:49 +0530

    project15

---
+++ project15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace project15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Stack s= new Stack();
+            s.Push(1);
+            s.Push(2);
+            s.Push(3);
+            foreach(var x in s){
+                Console.Write("{0} ",x);
+            }
+            Console.WriteLine();
+            s.Pop();
+            s.Push(4);
+            foreach(var x in s){
+                Console.Write("{0} ",x);
+            }
+            Console.WriteLine();
+        }
+    }
+}
