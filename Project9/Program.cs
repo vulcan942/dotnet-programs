ea044748842826fd4c9fabbc24b87be3906f8906
Vulcan942 <ncnishant9868@gmail.com>
2018-09-24 21:42:06 +0530

    Project9

---
+++ Project9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Project9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int grade=2;
+            Console.WriteLine();
+            switch(grade){
+
+                case 1: Console.WriteLine("Excellent"); break;
+                case 2: break;
+                default : Console.WriteLine("Passed"); break;
+            }     
+            Console.WriteLine();
+        }
+    }
+}
