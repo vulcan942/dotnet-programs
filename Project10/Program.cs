2ef418079a7708617fdb37048493f884abcfbf2d
Vulcan942 <ncnishant9868@gmail.com>
2018-09-24 21:59:28 +0530

    Project10

---
+++ Project10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+namespace Project10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           StringBuilder s= new StringBuilder("Hello, World");
+           Console.WriteLine();
+           Console.WriteLine("Original: {0} ",s);
+           s.Append('4'); 
+           Console.WriteLine("After append: {0}",s);
+           s.Insert(5,'5');
+           Console.WriteLine("After Insert: {0}",s);   
+           Console.WriteLine();        
+        }
+    }
+}
