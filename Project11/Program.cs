87a4d5b04a441dac427ef093bc74f82b758cb0f0
Vulcan942 <ncnishant9868@gmail.com>
2018-09-24 22:49:47 +0530

    Project11

---
+++ Project11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Project11
+{
+    class Program
+    {
+        public void print(){
+            Console.WriteLine("Hello, World");
+
+        }
+        public int print(int a,int b){
+            return  a + b;
+        }
+        static void Main(string[] args)
+        {
+            Program p=new Program();
+            Console.WriteLine();
+            p.print();
+            Console.WriteLine(p.print(5,6));
+            Console.WriteLine();
+        }
+    }
+}
