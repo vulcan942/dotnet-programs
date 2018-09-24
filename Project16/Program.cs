78025f69c89321367caee16e63b84b2be5b4d92b
Vulcan942 <ncnishant9868@gmail.com>
2018-09-25 00:03:15 +0530

    project16

---
+++ Project16/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace Project16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Hashtable ht = new Hashtable();
+            ht.Add("Quantity","Furniture");
+            ht.Add(250,"chair");
+            ht.Add(50,"table");
+            ht.Add(20,"Bed");
+
+            foreach(var x in ht.Keys){
+                Console.WriteLine("{0}=>{1}",x,ht[x]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
