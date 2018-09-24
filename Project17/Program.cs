3eb0b9344b34f125f078e7879f866cadbf65b994
Vulcan942 <ncnishant9868@gmail.com>
2018-09-25 00:16:23 +0530

    project17

---
+++ Project17/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+namespace Project17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Queue q= new Queue();
+            q.Enqueue(1);
+            q.Enqueue(5);
+            q.Enqueue(7);
+            q.Enqueue(3);
+            Console.WriteLine("No. of elements in Queue: {0}",q.Count);
+            Console.WriteLine();
+        }
+    }
+}
