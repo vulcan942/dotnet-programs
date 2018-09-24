0694d95ef5f97142e8e2da9e16d3166daaa77b05
Vulcan942 <ncnishant9868@gmail.com>
2018-09-24 23:40:03 +0530

    project14

---
+++ Project14/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+
+namespace Project14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            ArrayList list= new ArrayList();
+            list.Add(54);
+            list.Add('a');
+            list.Add(66.6);
+            foreach(var x in list){
+                Console.WriteLine(x);
+            }
+            Console.WriteLine();
+        }
+    }
+}
