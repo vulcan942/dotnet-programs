0a45e5f92db054c11cbc1ad2b2c6b54d7cd183e5
Vulcan942 <ncnishant9868@gmail.com>
2018-09-24 23:29:32 +0530

    project13

---
+++ Project13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Project13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            String s="ACDEB";
+            char[] c=s.ToCharArray(0,s.Length);
+            foreach(char x in c.OrderBy(y => y)){
+                Console.Write(x);
+            }
+            Console.WriteLine();
+            foreach(char x in c.Reverse()){
+                
+                Console.Write(x);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
